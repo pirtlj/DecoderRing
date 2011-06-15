9ceb46679871db96d3aace916801f955adf85f4f
pirtlj <pirtlj@gmail.com>
2011-06-14 18:50:08 -0700

    fix bug / refactor

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,27 +5,34 @@ class Message < ActiveRecord::Base
   
   require 'random_word_generator'
   
+  
+  class << self
+    public  
+      def code_regex
+        "/(?!Code #)\\b\\d+\\b(?= :)/"
+      end
+    
+  end
+  
+  public  
+    def encoded
+      "Code ##{self.id} : #{self.code}"
+    end
+  
   private
     def post_encoded 
-      
-      
+          
       self.code = RandomWordGenerator.composed(3, 15)
       self.save
       
       fb_user = FbGraph::User.me(self.user.fb_user_token.token)
       
-      begin
-      # something potentially bad
-      
-        fb_user.feed!(
-          :message => "Code Message ##{self.id} : #{self.code}",
-          :link => 'http://github.com/nov/fb_graph',
-          :name => 'FbGraph',
-          :description => 'A Ruby wrapper for Facebook Graph API'
-        )
-      rescue Exception=>e
-      # handle e
-      end
+      fb_user.feed!(
+        :message => self.encoded,
+        :link => 'http://github.com/nov/fb_graph',
+        :name => 'FbGraph',
+        :description => 'A Ruby wrapper for Facebook Graph API'
+      )
       
     end
   
